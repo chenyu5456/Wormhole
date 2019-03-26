216a84f1572e778cca8c3da681c6c8c53e35a65b
chenyu5456 <545641154@qq.com>
2019-03-26 15:27:57 +0800

    新增

---
+++ Wormhole/src/main/java/com/wormhole/framework/wormhole/WormholeApplication.java
@@ -0,0 +1,13 @@
+package com.wormhole.framework.wormhole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WormholeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WormholeApplication.class, args);
+    }
+
+}
